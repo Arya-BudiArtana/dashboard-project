39d2bc311bfba11477627f9fedadcd4de6c53cae
Arya-BudiArtana <aryabudiartana69@gmail.com>
2022-04-01 12:06:12 +0800

    add product

---
+++ orders.js
@@ -0,0 +1,45 @@
+const Orders = [
+    {
+        productName: 'Mechanical Keyboard',
+        productNumber: '1453',
+        paymentStatus: 'Paid',
+        shipping: 'Pending'
+    }, 
+    {
+        productName: 'RGB Keyboard',
+        productNumber: '8713',
+        paymentStatus: 'Unpaid',
+        shipping: 'Declined'
+    }, 
+    {
+        productName: 'RGB Mouse',
+        productNumber: '9841',
+        paymentStatus: 'Paid',
+        shipping: 'Succes'
+    }, 
+    {
+        productName: 'Monitor 24" ASUS',
+        productNumber: '5978',
+        paymentStatus: 'Paid',
+        shipping: 'Pending'
+    }, 
+    {
+        productName: 'Twisted Cable 2 Meters',
+        productNumber: '7753',
+        paymentStatus: 'Paid',
+        shipping: 'Succes'
+    }, 
+    {
+        productName: 'Gaming Pad',
+        productNumber: '2453',
+        paymentStatus: 'Unpaid',
+        shipping: 'Declined'
+    }, 
+    {
+        productName: 'Mechanical Keyboard',
+        productNumber: '1453',
+        paymentStatus: 'Paid',
+        shipping: 'Pending'
+    }, 
+
+]
